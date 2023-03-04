d1a538bf09c8b2bc692bbda31f7470330212f608
ditod1 <orlandodiazconde@gmail.com>
2023-03-04 07:17:53 -0500

    Update styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 
 const Layout = () => (
-  <div className="wrapper">
+  <div className="wrapper container">
     <Navbar />
     <Outlet />
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,34 @@ const links = [
 ];
 
 const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      {links.map((link) => (
-        <li key={link.text}>
-          <NavLink to={link.path}>{link.text}</NavLink>
-        </li>
-      ))}
-    </ul>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container">
+      <a className="navbar-brand" href="/">
+        Todo App
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          {links.map((link) => (
+            <li className="nav-item" key={link.text}>
+              <NavLink className="nav-link" to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 export default Navbar;
