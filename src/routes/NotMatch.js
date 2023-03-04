e3e4a39fdfd742e3f085fdd95793fd20f497e4d4
ditod1 <orlandodiazconde@gmail.com>
2023-03-04 12:12:20 -0500

    Add return to home button

--- src/routes/NotMatch.js
+++ src/routes/NotMatch.js
@@ -1,6 +1,9 @@
 const NotMatch = () => (
-  <div>
-    <p>Not found!</p>
+  <div className="d-flex align-items-center justify-content-center flex-column min-vh-100">
+    <p>Not found! Under Construction</p>
+    <a href="/" className="btn btn-info btn-pill" role="button">
+      Go Back Home
+    </a>
   </div>
 );
 export default NotMatch;
